72589e6c5280c3b3559d4954edc09e0f8463165d
dongzhaoqi <dong18765203770@163.com>
2016-07-04 02:11:11 +0800

    first

--- app/src/main/java/com/xutilstest/activity/MainActivity.java
+++ app/src/main/java/com/xutilstest/activity/MainActivity.java
@@ -6,7 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.xutilstest.CustomApplication;
 import com.xutilstest.R;
+import com.xutilstest.db.News;
+
+import org.xutils.DbManager;
+import org.xutils.ex.DbException;
+import org.xutils.x;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,18 +27,20 @@ public class MainActivity extends AppCompatActivity {
     Button btnNetwork;
     @Bind(R.id.btn_database)
     Button btnDatabase;
+    @Bind(R.id.btn_delete)
+    Button btnDelete;
+    private DbManager db;
+    private List<News> newsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
     }
 
 
-
-    @OnClick({R.id.btn_network, R.id.btn_database})
+    @OnClick({R.id.btn_network, R.id.btn_database,R.id.btn_delete})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_network:
@@ -39,6 +49,28 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_database:
                 startActivity(new Intent(this, DbActivity.class));
                 break;
+            case R.id.btn_delete:
+                delete();
+                break;
         }
     }
+
+    public void delete(){
+        db = x.getDb(CustomApplication.getInstance().getDaoConfig());
+
+        try {
+            newsList = db.selector(News.class).findAll();
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
+        for (News news : newsList){
+            try {
+                db.delete(news);
+            } catch (DbException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
